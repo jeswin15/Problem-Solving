018b29106cba439171ba2772b2945e1fe65f2fc7
jeswin15 <jeswin1564@gmail.com>
2024-11-05 23:45:06 +0530

    Menucard.java

--- Menucard.java
+++ Menucard.java
@@ -1,3 +1,17 @@
+//Question
+//Naveen Kumar is ordering food in menucard. In menucard below items are listed.
+
+//Pizza -  200
+//Burger -  150
+//Biriyani - 250
+//Dosa - 80
+//Idly - 50
+
+//Calculate the quantity of the items and display if the total price is exceeds more than 500, if yes discount applicable for 10%.
+//Finally after the discount display the proper total price.
+
+
+
 import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
